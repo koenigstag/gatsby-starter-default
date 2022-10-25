fa94578d44766bbba4b689c61828140dd50cc83f
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-10-25 21:31:46 +0300

    Add dynamic country pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
@@ -6,4 +6,34 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+
+  const { data } = await graphql(`
+    query AllCountries {
+      CountryAPI {
+        countries {
+          name
+          code
+          native
+          capital
+          emoji
+          currency
+          languages {
+            code
+            name
+          }
+        }
+      }
+    }
+  `);
+
+  data?.CountryAPI?.countries?.forEach(country => {
+    createPage({
+      path: `${country.name.toLowerCase().replace(/ /g, '-')}`,
+      component: require.resolve("./src/templates/[country].js"),
+      context: {
+        code: country.code,
+      },
+      // defer: true,
+    })
+  });
 }
---
+++ src/templates/[country].js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+
+const CountryPage = (props) => {
+  console.log(props.data?.CountryAPI?.country);
+
+  const data = props.data?.CountryAPI?.country;
+
+  return (<Layout>
+    <div>
+      <h1>Info about {data.emoji} {data.name}</h1>
+      <ul style={{ listStyle: 'none' }}>
+        <li>Capital: <b>{data.capital}</b></li>
+        <li>Continent: <b>{data.continent?.name}</b></li>
+        <li>Native: <b>{data.native}</b></li>
+        <li>Currency: <b>{data.currency}</b></li>
+        <li>Phone: <b>{data.phone}</b></li>
+        <li>Code: <b>{data.code}</b></li>
+        <li>Languages: <b>{data.languages?.map(lang => lang.name)?.join(', ')}</b></li>
+      </ul>
+    </div>
+  </Layout>);
+};
+
+export const query = graphql`
+  query CountryInfo ($code: ID!) {
+    CountryAPI {
+      country(code: $code) {
+        code
+        name
+        native
+        capital
+        phone
+        emoji
+        currency
+        continent {
+          name
+        }
+        languages {
+          code
+          name
+        }
+      }
+    }
+  }
+`
+
+export default CountryPage;
